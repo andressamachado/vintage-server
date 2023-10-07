67c4285b3315eb250c7661c96195bde4687f6790
andressamachado <andressa.pamachado@gmail.com>
2023-10-07 17:46:04 -0400

    Update knexfile to get client value from .env file

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,16 +1,17 @@
 // Import dotenv to process environment variables from `.env` file.
 require("dotenv").config({ path: "../.env" });
 
+// console.log(process.env.DB_HOST, "DB_HOST");
+
 module.exports = {
-  development: {
-    client: "mysql2",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      charset: "utf8",
-    },
+  client: process.env.DB_CLIENT,
+  connection: {
+    client: process.env.DB_CLIENT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    charset: "utf8",
   },
   migrations: {
     directory: "./migrations",
