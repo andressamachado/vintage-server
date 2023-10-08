c6a7f98dfcefb534e3874ef79c13d83ae043985e
andressamachado <andressa.pamachado@gmail.com>
2023-10-08 12:16:17 -0400

    Add a route to get a user by its id from the Database

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -18,6 +18,37 @@ const getAllUsers = async (req, res) => {
   }
 };
 
+// GET http://127.0.0.1:5050/api/users/:userId
+// Returns a user from the Database.
+const getUserById = async (req, res) => {
+  try {
+    // SELECT returns a list of instances found in the database.
+    const user = await knex
+      .select("first_name", "last_name", "email", "phone", "address", "isAdmin")
+      .from("users")
+      .where("id", req.params.userId);
+
+    // No user associated with the ID.
+    if (user.length === 0) {
+      res.status(404).send(`User with ID [ ${req.params.userId} ] not found`);
+    }
+
+    // If more than one user is found, return an error.
+    if (user.length > 1) {
+      res
+        .status(500)
+        .send(
+          `ID conflict. Multiple users found with ID [${req.params.userId}]`
+        );
+    }
+
+    res.status(200).json(user[0]);
+  } catch (err) {
+    res.status(404).send(`Error retrieving items: ${err}`);
+  }
+};
+
 module.exports = {
   getAllUsers,
+  getUserById,
 };
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,4 +4,7 @@ const userController = require("../controllers/user-controller");
 // eg. GET http://127.0.0.1:5050/api/users/
 router.route("/").get(userController.getAllUsers);
 
+// eg. GET http://127.0.0.1:5050/api/users/:userId
+router.route("/:userId").get(userController.getUserById);
+
 module.exports = router;
