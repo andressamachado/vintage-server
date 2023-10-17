52a53e315b407dabbd0a7d782f7f44a599544134
andressamachado <andressa.pamachado@gmail.com>
2023-10-17 11:58:38 -0400

    last minute submition

---
+++ database/migrations/20231017135825_create-user-product-table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("wishlist", (table) => {
+    table.increments("id").primary();
+    table.string("user_id").notNullable();
+    table.string("product_id").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("wishlist");
+};
